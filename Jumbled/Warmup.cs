16c5295bbbb3da34c360183d2cbc504576b5009d
Sagar Thakore <sagart12@gmail.com>
2024-05-23 00:20:13 -0400

    add warmup endpoint

---
+++ Jumbled/Warmup.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+
+namespace Jumbled
+{
+    public class Warmup()
+    {
+        [Function("Warmup")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+        {
+            return new OkObjectResult("ready");
+        }
+    }
+}
