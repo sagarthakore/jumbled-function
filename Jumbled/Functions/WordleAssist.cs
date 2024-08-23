56fb6df1677fbdde0e0b2724125ce3304b32e7e4
Sagar Thakore <sagart12@gmail.com>
2024-08-22 23:31:15 -0400

    track request to application insights custom telemetry

--- Jumbled/Functions/WordleAssist.cs
+++ Jumbled/Functions/WordleAssist.cs
@@ -1,4 +1,6 @@
 using Jumbled.Services.Interfaces;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -6,15 +8,28 @@ using Microsoft.Extensions.Logging;
 
 namespace Jumbled.Functions;
 
-public class WordleAssist(ILogger<WordleAssist> logger, IWordleAssistService wordleAssist)
+public class WordleAssist(ILogger<WordleAssist> logger, IWordleAssistService wordleAssist, TelemetryConfiguration telemetryConfiguration)
 {
     private readonly ILogger<WordleAssist> _logger = logger;
     private readonly IWordleAssistService _wordleAssist = wordleAssist;
+    private readonly TelemetryClient _telemetryClient = new(telemetryConfiguration);
 
     [Function("WordleAssist")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
     {
         _logger.LogInformation("Request Received - {query}", req.Query);
+        
+        _telemetryClient.TrackEvent("WordleAssist Request Received", new Dictionary<string, string> {
+            {
+                "word", req.Query["word"]!.ToString()
+            },
+            {
+                "exclude", req.Query["exclude"]!.ToString()
+            },
+            {
+                "include", req.Query["include"]!.ToString()
+            }
+        });
 
         string word = req.Query["word"].ToString().ToLowerInvariant();
         string exclude = req.Query["exclude"].ToString().ToLowerInvariant();
