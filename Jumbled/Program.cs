d60a4ed8bc8be6a4aa57893297b784ebf58a8c12
Sagar Thakore <sagart12@gmail.com>
2026-04-19 00:28:35 -0400

    refactor: convert Program.cs to explicit class excluded from coverage

--- Jumbled/Program.cs
+++ Jumbled/Program.cs
@@ -1,18 +1,28 @@
+using System.Diagnostics.CodeAnalysis;
 using Jumbled.Services;
 using Jumbled.Services.Interfaces;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
-    {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-        services.AddSingleton<IWordleAssistService, WordleAssistService>();
-        services.AddHealthChecks();
-    })
-    .Build();
+namespace Jumbled;
 
-await host.RunAsync();
+[ExcludeFromCodeCoverage(Justification = "Host bootstrap has no branching logic to assert")]
+public static class Program
+{
+    public static async Task Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWebApplication()
+            .ConfigureServices(services =>
+            {
+                services.AddApplicationInsightsTelemetryWorkerService();
+                services.ConfigureFunctionsApplicationInsights();
+                services.AddSingleton<IWordleAssistService, WordleAssistService>();
+                services.AddHealthChecks();
+            })
+            .Build();
+
+        await host.RunAsync();
+    }
+}
